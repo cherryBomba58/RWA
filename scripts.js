534a10bdd8e6bb04266b4a9408371b9ec9dd69e5
Répássy Adrienn <cherryBomba58@users.noreply.github.com>
2016-02-29 18:31:46 +0100

    Can add rows to the table now! Two working versions of JavaScript Button has to be outside of form

--- scripts.js
+++ scripts.js
@@ -1,34 +1,28 @@
 ﻿function addRow() {
+
 	var trr = document.createElement("tr");
 	
 	
 	var td1 = document.createElement("td");
-	var w1 = document.createTextNode(document.getElementById("code").value);
-	td1.appendChild(w1);		
+	td1.innerHTML = document.getElementById("code").value;
 		
 	var td2 = document.createElement("td");
-	var w2 = document.createTextNode(document.getElementById("name").value);
-	td2.appendChild(w2);
+	td2.innerHTML = document.getElementById("name").value;
 		
 	var td3 = document.createElement("td");
-	var w3 = document.createTextNode(document.getElementById("credit").value);
-	td3.appendChild(w3);
+	td3.innerHTML = document.getElementById("credit").value;
 	
 	var td4 = document.createElement("td");
-	var w4 = document.createTextNode(document.getElementById("teacher").value);
-	td4.appendChild(w4);
-	
+	td4.innerHTML = document.getElementById("teacher").value;
+		
 		
 	trr.appendChild(td1);
 	trr.appendChild(td2);
 	trr.appendChild(td3);
 	trr.appendChild(td4);
 	
+	
 	var tab = document.getElementById("tab");
 	tab.appendChild(trr);
 	
-	var di = document.getElementById("di");
-	di.appendChild(tab);
-	
-	document.appendChild(di);
 }
---
+++ scripts2.js
@@ -0,0 +1,17 @@
+﻿function addRow() {
+	
+	var table = document.getElementById("tab");
+	
+	var row = table.insertRow(-1);	// inserts row at the last position
+	
+	var cell0 = row.insertCell(0);
+	var cell1 = row.insertCell(1);
+	var cell2 = row.insertCell(2);
+	var cell3 = row.insertCell(3);
+	
+	cell0.innerHTML = document.getElementById("code").value;
+	cell1.innerHTML = document.getElementById("name").value;
+	cell2.innerHTML = document.getElementById("credit").value;
+	cell3.innerHTML = document.getElementById("teacher").value;
+	
+}
