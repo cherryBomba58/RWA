5c372bea0393bc38f2822209b9722b02cc58e286
Répássy Adrienn <cherryBomba58@users.noreply.github.com>
2016-03-09 00:54:10 +0100

    First steps to Angular1 Table stored and loaded smarter

---
+++ 2-Subject uploader Bootstrap+Angular1/myControl.js
@@ -0,0 +1,10 @@
+﻿var app = angular.module("myApp", []);
+app.controller("myControl", function($scope) {
+	$scope.subjects = [
+		{code:'BMEVIAUA300',name:'Önálló laboratórium',credit:'6',teacher:'Gipsz Jakab'},
+		{code:'BMEVIAUA350',name:'Informatikai technológiák',credit:'2',teacher:'Tóth László'},
+		{code:'BMEVIAUA360',name:'Adatvezérelt alkalmazások',credit:'4',teacher:'Szabó Zoltán'},
+		{code:'RXFVKBTA270',name:'Bűbájtan',credit:'2',teacher:'Perseus Piton'},
+		{code:'HRVBKRTA120',name:'Történelem',credit:'5',teacher:'ifj. Henry Jones'}
+	];
+});
