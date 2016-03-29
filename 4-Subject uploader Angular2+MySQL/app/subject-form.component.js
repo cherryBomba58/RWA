aa8718fc3b4312a2a38dcebea742cf702209cbd7
Répássy Adrienn <cherryBomba58@users.noreply.github.com>
2016-03-30 00:47:19 +0200

    New buttons added, DB coming soon

---
+++ 4-Subject uploader Angular2+MySQL/app/subject-form.component.js
@@ -0,0 +1,29 @@
+﻿(function(app) {
+  app.SubjectFormComponent = ng.core
+    .Component({
+      selector: 'subject-form',
+      templateUrl: 'app/subject-form.component.html'
+    })
+    .Class({
+      constructor: function() {
+        this.subjects = [
+        	new app.Subject('BMEVIAUA300', 'Önálló laboratórium', '6', 'Gipsz Jakab'), 
+        	new app.Subject('BMEVIAUA350', 'Informatikai technológiák', '2', 'Tóth László'), 
+        	new app.Subject('BMEVIAUA360', 'Adatvezérelt alkalmazások', '4', 'Szabó Zoltán'), 
+        	new app.Subject('RXFVKBTA270', 'Bűbájtan', '2', 'Perseus Piton'), 
+        	new app.Subject('HRVBKRTA120', 'Történelem', '5', 'ifj. Henry Jones')
+        	// here goes a select from
+        ];
+      },
+      addRow: function(code, name, credit, teacher) {
+        this.subjects.push(new app.Subject(code, name, credit, teacher));
+        // here goes an insert into
+      },
+      modRow: function(code, name, credit, teacher) {
+		// here goes an update set
+      },
+	  delRow: function(code, name, credit, teacher) {
+		// here goes a delete from
+      }
+    });
+})(window.app || (window.app = {}));
