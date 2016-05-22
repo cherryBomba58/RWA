7c123a053d4aaba265297a7f2fa09aedefc54979
Répássy Adrienn <cherryBomba58@users.noreply.github.com>
2016-05-22 11:46:30 +0200

    Error message if ISBN isn't given

--- 5b-Book lending site Angular1/public/app/myDataControl.js
+++ 5b-Book lending site Angular1/public/app/myDataControl.js
@@ -40,38 +40,44 @@ app.controller("myDataControl", ['$scope','$http','$cookies',function($scope, $h
 		 });
 	
 	$scope.addLending = function() {
-		// könyv felvétele a könyvek adatbázisába, ha még nincs ott
-		$http.get('/api/books/' + $scope.ISBN)
-	    	.success(function(data) {
-	        	console.log(data);
-	        	if(data[0] == null) {
-	        		$scope.book = {writer:$scope.writer,title:$scope.title,year:$scope.year,
-						publisher:$scope.publisher,p_year:$scope.p_year,ISBN:$scope.ISBN};
-						
-					console.log($scope.book);
-						
-			        $http.post('/api/books', $scope.book)
+		if($scope.ISBN == null) {
+			$scope.successmessage = 'Kérlek add meg a könyv ISBN azonosítóját!';
+		}
+		else {
+			// könyv felvétele a könyvek adatbázisába, ha még nincs ott
+			$http.get('/api/books/' + $scope.ISBN)
+		    	.success(function(data) {
+		        	console.log(data);
+		        	if(data[0] == null) {
+		        		$scope.book = {writer:$scope.writer,title:$scope.title,year:$scope.year,
+							publisher:$scope.publisher,p_year:$scope.p_year,ISBN:$scope.ISBN};
+							
+						console.log($scope.book);
+							
+				        $http.post('/api/books', $scope.book)
+				            .success(function(data) {
+				                console.log(data);
+				            })
+				            .error(function(data) {
+				                console.log('Error: ' + data);
+				            });
+		        	}
+		        	
+		        	$scope.lend = {offerid:null,lender:myusername,borrower:null,bookid:$scope.ISBN};
+				
+					$http.post('/api/offers', $scope.lend)
 			            .success(function(data) {
+			                $scope.lendings = data;
 			                console.log(data);
+			                $scope.successmessage = 'Sikerült az ajánlat felvétele!';
 			            })
 			            .error(function(data) {
 			                console.log('Error: ' + data);
 			            });
-	        	}
-	        	
-	        	$scope.lend = {offerid:null,lender:myusername,borrower:null,bookid:$scope.ISBN};
-			
-				$http.post('/api/offers', $scope.lend)
-		            .success(function(data) {
-		                $scope.lendings = data;
-		                console.log(data);
-		            })
-		            .error(function(data) {
-		                console.log('Error: ' + data);
-		            });
-	        })
-	        .error(function(data) {
-	        	console.log('Error: ' + data);
-	        });
+		        })
+		        .error(function(data) {
+		        	console.log('Error: ' + data);
+		        });
+		}
 	}
 }]);
